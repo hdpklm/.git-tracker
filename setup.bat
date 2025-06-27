a247425ae2f2cfd43c09dfe9e3b049980e23e9a1
hdpklm <hdaoud10@gmail.com>
2025-06-27 13:29:40 +0200

    123

--- setup.bat
+++ setup.bat
@@ -1,7 +1,7 @@
 @echo off
-git config --global init.templatedir "%USERPROFILE%/.git-tracker"
+git config --global core.hooksPath "%USERPROFILE%/.git-templates/hooks"
 echo Git hooks configurados correctamente
 echo.
-echo Configuracion actual:
-git config --global --get init.templatedir
+echo Configuracion actual "core.hooksPath":
+git config --global --get core.hooksPath
 pause
